482301978b6d58c290c71b9d1a77b024f144f583
jiangdmv <jiangdmv@gmail.com>
2022-05-01 15:32:56 -0400

    5/1

---
+++ Interview-Questions/test.js
@@ -0,0 +1,17 @@
+
+
+var minimumCardPick = function (cards) {
+  if (cards.length < 2) {
+    return -1;
+  }
+  arr = [];
+  arr.push(cards[0]);
+  length=-1
+
+  for (let i=1; i<cards.length, i++){
+    arr.push(cards[i]);
+    
+  }
+
+
+};
